999dcda9b8b94545a534e84fe23cc12496a56f16
myfriendkang <myfriendkang@gmail.com>
2016-07-18 21:28:45 -0700

    initial commit

---
+++ Assets/2. Script/CellController.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CellController : MonoBehaviour {
+
+    public GameObject[] greenCells;
+    private GameObject _greenCellSelected;
+    public Text waterText;
+    public Text phText;
+    public Text nutrientText;
+    public Text cellName;
+    int water;
+    int ph;
+    int nutrient;
+    int value;
+    // Use this for initialization
+    void Start() {
+        water = 0;
+        ph = 0;
+        nutrient = 0;
+        value = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        CheckWhichCellSelected();
+    }
+    void CheckWhichCellSelected()
+    {
+        foreach (GameObject cell in greenCells)
+        {
+            if (cell.GetComponent<GreenCellStatus>().selectCell == true)
+            {
+                Debug.Log(cell.gameObject.GetComponent<GreenCellStatus>().GetNameOfCell());
+                _greenCellSelected = cell;
+                cellName.text = _greenCellSelected.GetComponent<GreenCellStatus>().GetNameOfCell();
+                water = _greenCellSelected.GetComponent<GreenCellStatus>().GetWaterOfCell();
+                ph = _greenCellSelected.GetComponent<GreenCellStatus>().GetPhOfCell();
+                nutrient = _greenCellSelected.GetComponent<GreenCellStatus>().GetNutrientOfCell();
+                waterText.text = water.ToString();
+                phText.text = ph.ToString();
+                nutrientText.text = nutrient.ToString();
+
+            }
+           cell.gameObject.GetComponent<GreenCellStatus>().SetSelected();
+        }
+    }
+    #region controlValue
+    public void Water_OnPlusButtonClick()
+    {
+        water++;
+        waterText.text = water.ToString();
+    }
+
+    public void Water_OnMinusButtonClick()
+    {
+        water--;
+        waterText.text = water.ToString();
+    }
+
+    public void Water_OnOKButtonClick()
+    {
+        _greenCellSelected.GetComponent<GreenCellStatus>().SetWaterValue(water);
+        _greenCellSelected.GetComponent<GreenCellStatus>().waterLevel = water;
+        _greenCellSelected.GetComponent<GreenCellStatus>().display[2].GetComponentInChildren<Text>().text = "Water : " + water.ToString();
+        
+        
+    }
+    public void PH_OnPlusButtonClick()
+    {
+        ph++;
+        phText.text = ph.ToString();
+    }
+
+    public void PH_OnMinusButtonClick()
+    {
+        ph--;
+        phText.text = ph.ToString();
+    }
+
+    public void PH_OnOKButtonClick()
+    {
+        _greenCellSelected.GetComponent<GreenCellStatus>().SetPHValue(ph);
+        _greenCellSelected.GetComponent<GreenCellStatus>().phLevel = ph;
+        _greenCellSelected.GetComponent<GreenCellStatus>().display[3].GetComponentInChildren<Text>().text = "PH    : " + ph.ToString();
+    }
+    public void Nut_OnPlusButtonClick()
+    {
+        nutrient++;
+        nutrientText.text = nutrient.ToString();
+    }
+
+    public void Nut_OnMinusButtonClick()
+    {
+        nutrient--;
+        nutrientText.text = nutrient.ToString();
+    }
+
+    public void Nut_OnOKButtonClick()
+    {
+        _greenCellSelected.GetComponent<GreenCellStatus>().SetNutrientValue(nutrient);
+        _greenCellSelected.GetComponent<GreenCellStatus>().sunLevel = nutrient;
+        _greenCellSelected.GetComponent<GreenCellStatus>().display[4].GetComponentInChildren<Text>().text = "Sun   : " + nutrient.ToString();
+    }
+    #endregion
+}
---
+++ Assets/2. Script/GreenCellEvent.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GreenCellEvent : MonoBehaviour {
+    #region CellInfo Calss
+    public class CellInfo
+    {
+
+        private string _name;
+        private string _date;
+        private float _water;
+        private float _ph;
+        private float _sun;
+
+        public CellInfo(string name, string date, float water, float phLevel, float sunLight)
+        {
+            _name = name;
+            _date = date;
+            _water = water;
+            _ph = phLevel;
+            _sun = sunLight;
+        }
+
+        public void SetName(string str)
+        {
+            _name = str;
+        }
+        public void SetDate(string str)
+        {
+            _date = str;
+        }
+        public void SetWater(float amount)
+        {
+            _water = amount;
+        }
+        public void SetPH(float ph)
+        {
+            _ph = ph;
+        }
+        public void SetSunLight(float sun)
+        {
+            _sun = sun;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+        public string GetDate()
+        {
+            return _date;
+        }
+        public float GetWater()
+        {
+            return _water;
+        }
+        public float GetPH()
+        {
+            return _ph;
+        }
+        public float GetSun()
+        {
+            return _sun;
+        }
+    }
+    #endregion
+    public string nameStr;
+    public string dateStr;
+    public float waterLevel;
+    public float phLevel;
+    public float sunLevel;
+    public InputField[] display;
+    /*InputField[0] = Name
+      InputField[1] = Date
+      InputField[2] = Water
+      InputField[3] = PH
+      InputField[4] = Sun
+   */
+    CellInfo cellInfo = new CellInfo("_empty", "none", 0.0f, 0.0f, 0.0f);
+    bool selectCell = false;
+    // Use this for initialization
+    void Start()
+    {
+        selectCell = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetCellInfo();
+    }
+    public bool isSelected()
+    {
+        if (selectCell == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void SetCellInfo()
+    {
+        cellInfo.SetName(nameStr);
+        cellInfo.SetDate(dateStr);
+        cellInfo.SetPH(phLevel);
+        cellInfo.SetSunLight(sunLevel);
+        cellInfo.SetWater(waterLevel);
+    }
+
+    public void OnGreenCellClick()
+    {
+        display[0].GetComponentInChildren<Text>().text = "Name  : " + cellInfo.GetName();
+        display[1].GetComponentInChildren<Text>().text = "Date  : " + cellInfo.GetDate();
+        display[2].GetComponentInChildren<Text>().text = "Water : " + cellInfo.GetWater();
+        display[3].GetComponentInChildren<Text>().text = "PH    : " + cellInfo.GetPH();
+        display[4].GetComponentInChildren<Text>().text = "Sun   : " + cellInfo.GetSun();
+
+        if (selectCell == false)
+        {
+            selectCell = true;
+        }
+    }
+}
